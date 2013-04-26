b9941034b45d703bae89f38996cee518c4ce0ceb
cmdaltent <weissbach.martin@gmail.com>
2013-04-26 14:06:05 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebCall::Application.config.secret_token = '44d8a7080c64803692735f6e03987e14d953ed90fad7f8f4198be2351af8a2f0e0ae6319ddab49f144810b7849ff61c2aca18142a5524792abfa1acdd6ac62f4'
