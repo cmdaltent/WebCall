b2aa6c9cc5c1bbc240d7c7b955b3eafc98cbbbcb
cmdaltent <weissbach.martin@gmail.com>
2013-05-06 21:38:11 +0200

    [WEB-24]
    – relationships added to model
    – migration file added

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,8 @@
 class Meeting < ActiveRecord::Base
   attr_accessible :description, :expectedDuration, :startDate, :title
   
+  has_and_belongs_to_many :users
+  
   validates :title, :presence => true,
                     :length => { :minimum => 1}
   validates :startDate, :presence => true
