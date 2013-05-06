e73abc875ccc5e3d82df8d3b8ce3fe49a89595e1
cmdaltent <weissbach.martin@gmail.com>
2013-05-06 21:38:11 +0200

    [WEB-24]
    – relationships added to model
    – migration file added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :firstName, :lastName, :password, :username
+  
+  has_and_belongs_to_many :meetings
 
   validates :email, :presence => true,
   :length => {:minimum => 4},
