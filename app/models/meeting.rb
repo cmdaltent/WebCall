c5bb3d1e6e1440230d9b0819f8f998ad63a766d9
Peng Chen <p.chen88@hotmail.com>
2013-05-23 16:05:38 +0200

    validate the scheduled meetings.

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -6,9 +6,11 @@ class Meeting < ActiveRecord::Base
   
   
   before_save :create_token
+  #before_save :check_meeting_time?
   
   VALID_INTEGER_REGEX = /^[1-9]\d*$/
   
+  validate :check_meeting_time?
   validates :expectedDuration, :presence => true,:numericality => true
   
   validates :startDate,:presence => true, :numericality => true
@@ -20,4 +22,14 @@ class Meeting < ActiveRecord::Base
   def create_token
     self.token = SecureRandom.uuid
   end
+  
+  def check_meeting_time?
+    if !(self.startDate > Time.new.to_i)
+      errors.add(:startDate,"can't strat from this time,is greater than #{Time.now.to_i}.")
+    end
+    if !(self.expectedDuration > 0)
+      errors.add(:expectedDuration,"is a invalid duration time,#{Time.at(self.expectedDuration)}.")
+    end
+  end
+  
 end
