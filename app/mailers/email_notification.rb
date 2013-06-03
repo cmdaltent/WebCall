87fd00b152d756e0c5c16593b40955b52a5a7d4f
cmdaltent <weissbach.martin@gmail.com>
2013-06-03 18:25:47 +0200

    WEB-48 finished implementation

--- app/mailers/email_notification.rb
+++ app/mailers/email_notification.rb
@@ -1,7 +1,7 @@
 class EmailNotification < ActionMailer::Base
-  default :from => "no-replay@kpwebcall.com", :content_type => "text/html"
+  default :from => "no-replay@kpwebcall.com", :content_type => "text/html" 
   
   def meeting_notification(recipient, meeting, sent_at = Time.now)
-    mail(:to => recipient, :subject => "KP WebCall – meeting invitation", :body => meeting.token.to_s)
+    mail(:bcc => recipient, :subject => "KP WebCall – meeting invitation", :body => meeting.token.to_s)
   end
 end
