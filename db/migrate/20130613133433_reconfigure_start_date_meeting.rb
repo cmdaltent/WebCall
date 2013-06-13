488334c11146aedb5176302ca15773d8e493d7b6
mrVVoo <markus.wutzler@gmail.com>
2013-06-14 00:30:00 +0200

    modified migration to convert dates in order to avoid reset.

--- db/migrate/20130613133433_reconfigure_start_date_meeting.rb
+++ db/migrate/20130613133433_reconfigure_start_date_meeting.rb
@@ -1,9 +1,15 @@
 class ReconfigureStartDateMeeting < ActiveRecord::Migration
   def up
     change_column :meetings, :startDate, :datetime
+    Meeting.all.each do |meeting|
+      meeting.update_attributes!(:startDate => Time.at(meeting.startDate))
+    end
   end
 
   def down
     change_column :meetings, :startDate, :integer
+    Meeting.all.each do |meeting|
+      meeting.update_attributes!(:startDate => Time.to_i)
+    end
   end
 end
